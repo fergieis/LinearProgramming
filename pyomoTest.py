7991743e79d4f800ed8a4c5fc14737912f00033f
fergieis <fergieis@yahoo.com>
2016-01-20 21:17:11 -0500

    matplotlib plot complete (not so much for bokeh)

--- pyomoTest.py
+++ pyomoTest.py
@@ -23,3 +23,27 @@ def const_expression(p, i):
 	return sum(p.a[i,j] * p.x[j] for i in p.J) <= p.b[i]
 
 p.AxbConstraint = Constraint(p.I, rule=const_expression)
+from pyomo.environ import * 
+from __future__ import division 
+
+p = AbstractModel()
+
+p.m = Param(within=NonNegativeIntegers)
+p.n = Param(within=NonNegativeIntegers)
+
+p.I = Set()
+p.J = Set()
+
+p.a = Param(p.I, p.J)
+p.b = Param(p.I)
+p.c = Param(p.J)
+
+p.x = Var(p.J, domain=NonNegativeReals)
+def obj_function(p):
+	return summation(p.c, p.a)
+
+p.OBJ = Objective(rule=obj_function, sense=maximize)
+def const_expression(p, i):
+	return sum(p.a[i,j] * p.x[j] for i in p.J) <= p.b[i]
+
+p.AxbConstraint = Constraint(p.I, rule=const_expression)
